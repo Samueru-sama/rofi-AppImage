04c396c76278e0618ef53dbce60eabfa2407ea07
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-06 23:01:44 -0400

    Update rofi-appimage.sh

--- rofi-appimage.sh
+++ rofi-appimage.sh
@@ -10,9 +10,9 @@ mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
 # DOWNLOAD AND BUILD ROFI
 CURRENTDIR="$(readlink -f "$(dirname "$0")")" # DO NOT MOVE THIS
-version=$(wget -q https://api.github.com/repos/"$SITE"/releases -O - | grep browser_download_url | grep -i tar.gz | cut -d '"' -f 4 | head -1)
+version=$(wget -q https://api.github.com/repos/"$SITE"/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*rofi.*releases.*rofi.*tar.gz$' | head -1)
 wget "$version" && tar fx ./*tar* && cd ./rofi-* && meson --prefix "$CURRENTDIR" . build && meson compile -C build && meson install -C build || exit 1
-cd .. && find ./bin/rofi -type f -executable -exec sed -i -e "s|/usr|././|g" {} \; && sed -i '44,68d; s/DIRS=\${XDG_DATA_DIRS}//' ./bin/rofi-theme-selector && rm -rf ./rofi-* #
+cd .. && find ./bin/rofi -type f -executable -exec sed -i -e "s|/usr|././|g" {} \; && sed -i '44,68d; s/DIRS=\${XDG_DATA_DIRS}//' ./bin/rofi-theme-selector && rm -rf ./rofi-*
 
 # ROFI EMOJI DOESN'T COMPILE!!!
 #git clone https://github.com/Mange/rofi-emoji.git && cd ./rofi-emoji && autoreconf -i && ./configure --prefix="$CURRENTDIR" && make && make install || exit 1
@@ -25,21 +25,16 @@ cp ./share/applications/rofi.desktop ./"$APP".desktop && cp ./share/icons/*/*/*/
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(readlink -f "$(dirname "$0")")"
+DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
 export PATH="$PATH:$CURRENTDIR/bin"
 export XDG_DATA_DIRS="$CURRENTDIR/share:$XDG_DATA_DIRS"
 
-if [ ! -d "$XDG_DATA_HOME/rofi/themes" ] && [ ! -d "$HOME/.local/share/rofi/themes" ]; then
+if [ ! -d "$DATADIR/rofi/themes" ]; then
 	if [ "$1" = "--install-themes" ]; then
-		if [ -n "$XDG_DATA_HOME" ]; then
-			mkdir -p "$XDG_DATA_HOME/rofi"
-			cp -r "$CURRENTDIR/share/rofi/themes" "$XDG_DATA_HOME/rofi/themes"
-		else
-			mkdir -p "$HOME/.local/share/rofi"
-			cp -r "$CURRENTDIR/share/rofi/themes" "$HOME/.local/share/rofi/themes"
-		fi
+		mkdir -p "$DATADIR/rofi" && cp -r "$CURRENTDIR/share/rofi/themes" "$DATADIR/rofi/themes"
 	else
 		echo "No rofi themes directory found" & notify-send "No rofi themes directory found"
-		echo "run rofi --install-themes to add them to ~/.local/share/rofi or XDG_DATA_HOME/rofi" 
+		echo 'run rofi --install-themes to add them to ~/.local/share/rofi or $XDG_DATA_HOME/rofi'
 	fi
 fi
 
