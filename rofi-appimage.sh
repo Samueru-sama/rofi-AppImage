e928bf5c825178959e088ddd68de715719735938
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-29 13:29:03 -0400

    Update rofi-appimage.sh

--- rofi-appimage.sh
+++ rofi-appimage.sh
@@ -54,7 +54,7 @@ cat >> ./AppRun << 'EOF'
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
 DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
 export PATH="$CURRENTDIR/bin:$PATH"
-export XDG_DATA_DIRS="$CURRENTDIR/usr/share:/usr/share:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="$DATADIR:$XDG_DATA_DIRS"
 export GIO_MODULE_DIR="$CURRENTDIR"
 
 GDK_HERE="$(find "$CURRENTDIR" -type d -regex '.*gdk.*loaders' -print -quit)"
