b9e2fd0ce8720c75794bb400790af3c6883af012
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-29 00:41:59 -0400

    Update rofi-AppImage.sh

--- rofi-AppImage.sh
+++ rofi-AppImage.sh
@@ -1,38 +1,57 @@
 #!/bin/sh
-set -u
+set -eu
 APP=rofi
 APPDIR="$APP.AppDir"
 SITE="davatorium/rofi"
 EXEC="$APP"
+export ARCH="$(uname -m)"
+APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+SHARUN="https://bin.ajam.dev/$ARCH/sharun"
 
 # CREATE DIRECTORIES
-if [ -z "$APP" ]; then exit 1; fi
 mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
 
 # DOWNLOAD AND BUILD ROFI
 CURRENTDIR="$(dirname "$(readlink -f "$0")")" # DO NOT MOVE THIS
-git clone https://github.com/davatorium/rofi.git && cd ./rofi \
-&& meson --prefix "$CURRENTDIR/usr" . build && meson compile -C build && meson install -C build || exit 1
-cd .. && sed -i '44,68d; s/DIRS=\${XDG_DATA_DIRS}//' ./usr/bin/rofi-theme-selector && rm -rf ./rofi
+git clone https://github.com/davatorium/rofi.git ./rofi
+cd ./rofi
+meson --prefix "$CURRENTDIR/usr" . build
+meson compile -C build && meson install -C build
+cd ..
+sed -i '44,68d; s/DIRS=\${XDG_DATA_DIRS}//' ./usr/bin/rofi-theme-selector
+rm -rf ./rofi
+
+# ADD LIBRARIES
+mv ./usr/bin ./
+wget "$LIB4BN" -O ./lib4bin && wget "$SHARUN" -O ./sharun
+chmod +x ./lib4bin ./sharun
+HARD_LINKS=1 ./lib4bin ./bin/*
+rm -f ./lib4bin
 
 # DESKTOP & ICON
-echo "Categories=Utility;" >> ./usr/share/applications/rofi.desktop && echo "Categories=Utility;" >> ./usr/share/applications/rofi-theme-selector.desktop
-cp ./usr/share/applications/rofi.desktop ./"$APP".desktop && cp ./usr/share/icons/*/*/*/* ./$APP.svg && ln -s ./*.svg ./.DirIcon || exit 1
+find ./ -type f -regex ".*/applications/.*\.desktop" -exec cp {} ./ \;
+rm -f ./rofi-theme-selector.desktop || true
+find ./ -type f -regex ".*/icons/.*\.\(svg\|png\)" -exec cp {} ./ \;
+ln -s ./*.svg ./.DirIcon
+
+echo "Categories=Utility;" >> ./rofi.desktop
 
 # AppRun
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
 DATADIR="${XDG_DATA_HOME:-$HOME/.local/share}"
-export PATH="$PATH:$CURRENTDIR/bin"
-export XDG_DATA_DIRS="$XDG_DATA_DIRS:$CURRENTDIR/usr/share:/usr/share"
+export PATH="$CURRENTDIR/bin:$PATH"
+export XDG_DATA_DIRS="$CURRENTDIR/usr/share:/usr/share:$XDG_DATA_DIRS"
 
 if [ ! -d "$DATADIR/rofi/themes" ]; then
-	if [ "$1" = "--install-themes" ]; then
-		mkdir -p "$DATADIR/rofi" && cp -r "$CURRENTDIR/usr/share/rofi/themes" "$DATADIR/rofi/themes"
-	else
-		echo "No rofi themes directory found" & notify-send "No rofi themes directory found"
-		echo 'run rofi --install-themes to add them to ~/.local/share/rofi or $XDG_DATA_HOME/rofi'
+	mkdir -p "$DATADIR/rofi" || exit 1
+	if ! cp -rn "$CURRENTDIR/usr/share/rofi/themes" "$DATADIR/rofi/themes"; then
+		echo "No rofi themes directory found"
+		echo "Something went wrong because the AppImage should have copied them"
+		echo "to "$DATADIR/rofi/themes""
+		notify-send "No rofi themes directory found"
 	fi
 fi
 
@@ -40,28 +59,20 @@ if [ "$1" = "rofi-theme-selector" ]; then
 	"$CURRENTDIR/bin/rofi-theme-selector"
 fi
 
-"$CURRENTDIR/usr/bin/rofi" "$@"
+exec "$CURRENTDIR/bin/rofi" "$@"
 EOF
 chmod a+x ./AppRun
-export VERSION=$(./AppRun -v | awk 'FNR==3 {print $2}')
+export VERSION="$(./AppRun -v | awk 'FNR==1 {print $2; exit}')"
+cd ..
 
-# MAKE APPIMAGE
-LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
-cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && chmod a+x ./linuxdeploy \
-	&& ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC"
-
-# hack
-patchelf --set-rpath '/lib:/lib64:/lib/x86_64-linux-gnu:/usr/lib:$ORIGIN/../lib' "$APPDIR"/usr/bin/"$EXEC"
-
-# LIBFUSE3
+# MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
 wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./appimagetool || exit 1
 rm -f ./"$APPDIR"/rofi-theme-selector* # Why does this get created?
 
 # Do the thing!
-export ARCH=x86_64
 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 \
-  -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|rofi-AppImage|continuous|*x86_64.AppImage.zsync" \
-  ./"$APP".AppDir Rofi-"$VERSION"-"$ARCH".AppImage 
+	-n -u "gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|rofi-AppImage|continuous|*$ARCH.AppImage.zsync" \
+	./"$APP".AppDir Rofi-"$VERSION"-"$ARCH".AppImage
 [ -n "$APP" ] && mv ./*.AppImage* .. && cd .. && rm -rf ./"$APP" || exit 1
 echo "All Done!"
